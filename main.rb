8c3cf3b149c4b0462d5e86486113e10d2b00aeba
ouin85 <bouinten@yahoo.fr>
2020-04-16 23:56:18 +0200

    modify main

--- main.rb
+++ main.rb
@@ -8,6 +8,6 @@ cryptocurrencies_uri = 'https://coinmarketcap.com/all/views/all/'
 
 p upload_cryptocurrencies_from_(cryptocurrencies_uri)
 
-# uri = 'https://www.annuaire-des-mairies.com/val-d-oise.html'
+uri = 'https://www.annuaire-des-mairies.com/val-d-oise.html'
 
-# p upload_townhalls_emails_of_Val_d_Oise(uri)
+p upload_townhalls_emails_of_Val_d_Oise(uri)
